a87e0e849148527b99e503d1f1aabfba86da39cf
Iryna Dzhupyna <irdzhupy@c3a8c1.42berlin.de>
2026-05-12 10:47:52 +0200

    module 4

--- Module_04/ex3/ft_vault_security.py
+++ Module_04/ex3/ft_vault_security.py
@@ -1,33 +1,33 @@
-def secure_archive(file_name, string=None, new_content=None) -> tuple:
+def secure_archive(file_name, string=None,
+                   new_content=None) -> tuple[True | False, str]:
     print("=== Cyber Archives Security ===")
     try:
-        with open(file_name, string) as file:
-            if string == "r":
+        if string == "r":
+            with open(file_name, string) as file:
                 print("Using 'secure_archive' to read from regular file")
                 content = file.read()
                 return (bool(content), content)
 
-            elif string == "w":
-                print("Using 'secure_archive' "
-                      "to write previous content to a new file")
-                with open(file_name, "r") as file_1:
-                    content = file_1.read()
+        elif string == "w":
+            with open(file_name, string) as file:
+                file.write(new_content)
+                return (True, "Content successfully written to file")
 
-                with open("new_file.txt", "w") as file_2:
-                    file_2.write(content)
-
-                return (bool(content), content)
-    except (FileNotFoundError, NotADirectoryError, IsADirectoryError):
+    except (FileNotFoundError, NotADirectoryError):
         print("Using 'secure_archive' to read from nonexisting file")
-        return ("False", f"[Errno 2] No such file or directory: '{file_name}'")
+        return (False, "[Errno 2] No such file or directory")
+
+    except IsADirectoryError:
+        print("Using 'secure_archive' to read from directory not a file")
+        return (False, "[Errno 2] Is a directory, not a file")
+
     except PermissionError:
         print("Using 'secure_archive' to read from inaccessible file")
-        return ("False", f"[Errno 13] Permission denied: '{file_name}'")
+        return (False, f"[Errno 13] Permission denied: '{file_name}'")
 
 
 def main() -> None:
-    print(secure_archive("test.txt", "r"))
-    print(secure_archive("ancient_fragment.txt", "w"))
+    print(secure_archive("test.txt", "w", "hello"))
 
 
 if __name__ == "__main__":
